22a82fce4681cf45ea277cd40abcf59909fd9413
Jim Pick <jim@jimpick.com>
2024-05-29 10:58:10 -0700

    Convert EHID to CNAME

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/base32"
+	"encoding/hex"
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/uber/h3-go/v4"
 )
@@ -19,19 +23,30 @@ func main() {
 	// 8928308280fffff
 
 	// uxirkffr.test.hex.camp
-	hex := "8a5d11514b1ffff"
 
-	cell2 := h3.Cell(h3.IndexFromString(hex))
+	// str := "ONXW2ZJAMRQXIYJAO5UXI2BAAAQGC3TEEDX3XPY="
+	str := strings.ToUpper("uxirkffr")
+	data, err := base32.StdEncoding.DecodeString(str)
+	if err != nil {
+		log.Fatal(err)
+	}
+	hex2 := strings.ReplaceAll(fmt.Sprintf("8%-14s", hex.EncodeToString(data)), " ", "f")
+	fmt.Printf("hex: %v\n", hex2)
+
+	// hex3 := "8a5d11514b1ffff"
+	hex3 := hex2
+
+	cell2 := h3.Cell(h3.IndexFromString(hex3))
 	fmt.Printf("cell2 %s\n", cell2)
 	resolution := cell2.Resolution()
 	fmt.Printf("cell2 resolution %v\n", resolution)
 	base := cell2.BaseCellNumber()
-	fmt.Printf("cell2 base %v\n", base)
 	for i := resolution; i > 0; i-- {
 		parent := cell2.Parent(i)
 		childPos := parent.ChildPos(i - 1)
 		fmt.Printf("  %d: Parent %s %d: %d\n", i, parent.String(), parent.Resolution(), childPos)
 	}
+	fmt.Printf("cell2 base %v\n", base)
 }
 
 // 3.4.5.4.2.4.2.1.2.4.46.h3.test.hex.camp
